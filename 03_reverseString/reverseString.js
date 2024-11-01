521285ea29e9e45326d8909010a0a2019c2d8270
Victor chromebook <175169337+itsvictoroyedeji@users.noreply.github.com>
2024-11-01 06:17:10 -0400

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  // break up text into array into a new variable
+  const word = string.split("");
+
+  // reverse array (changes original array so no need to store in new variable)
+  word.reverse();
+
+  // join array and return result
+  return word.join("");
 
 };
 
