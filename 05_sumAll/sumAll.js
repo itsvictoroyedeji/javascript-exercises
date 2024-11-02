767349fec04ce4b51406c318ac12f3b7ad1327e3
Victor chromebook <175169337+itsvictoroyedeji@users.noreply.github.com>
2024-11-02 02:21:42 -0400

    '05_sumAll' complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,47 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  let sum = 0;
 
-};
+// Here's one way:
+//   if (Number.isInteger(start) && Number.isInteger(end) ) {
+//     if (start > 0 && end > 0) {
+//       if (start < end) {
+//         for (let i = start ; i <= end ; i++) {
+//           sum += i;
+//         }
+//       } else if (start > end) {
+//         for (let i = start ; i >= end ; i--) {
+//           sum += i;
+//         }
+//       } else {
+//         sum = 'ERROR';
+//       } 
+//     } else {
+//       sum = 'ERROR';
+//     }
+//   } else {
+//     sum = 'ERROR';
+//   }
+//   return sum;
+// }
 
+  // Here's the other:
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    return 'ERROR';
+  }
+  if (start < 0 || end < 0) {
+    return 'ERROR';
+  }
+
+  if (start < end) {
+    for (let i = start ; i <= end ; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = start ; i >= end ; i--) {
+      sum += i;
+    }
+  }
+  return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
