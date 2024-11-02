d41c8d73e7ebca1522be7822838fd1c20df897dc
Victor chromebook <175169337+itsvictoroyedeji@users.noreply.github.com>
2024-11-02 01:29:50 -0400

    '04_removeFromArray' complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+  let position = '';
+  
+  for (value of values) {
+    while (arr.includes(value)) {
+      position = arr.indexOf(value);
+      arr.splice(position,1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
